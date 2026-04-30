a95a39f39b1762f0f563c47569e3ede442b87d4d
Simonjs <34932488+truesimonjs@users.noreply.github.com>
2026-04-30 11:26:35 +0200

    Added some interfaces

---
+++ Models/IHasId.cs
@@ -0,0 +1,7 @@
+﻿namespace Datamatiker2SemExam.Models
+{
+    public interface IHasId
+    {
+        int Id { get; set; }
+    }
+}
---
+++ Services/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Datamatiker2SemExam.Models;
+
+namespace Datamatiker2SemExam.Services
+{
+    public interface IRepository<T> where T : IHasId
+    {
+        List<T> GetAll();
+        int Create(T entity);
+
+        T? Read (int id);
+
+        /// <summary>
+        /// Return true if element was deleted
+        /// </summary>
+      
+        bool Delete (int id);
+    }
+}
