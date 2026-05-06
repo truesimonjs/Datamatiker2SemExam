950dbbde5e34ba2d90412398b269f54eb58d6826
Simonjs <34932488+truesimonjs@users.noreply.github.com>
2026-05-06 12:23:38 +0200

    Added booking to EFC

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -14,4 +14,8 @@ public partial class Customer
     public string Email { get; set; }
 
     public string Phonenumber { get; set; }
+
+    public virtual ICollection<BookingHistory> BookingHistories { get; set; } = new List<BookingHistory>();
+
+    public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
 }
