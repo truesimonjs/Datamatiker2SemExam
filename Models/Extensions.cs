135aa26a3d88592f9f5a42a9f7ec931ccd34f000
Simonjs <34932488+truesimonjs@users.noreply.github.com>
2026-05-07 10:59:41 +0200

    don't have the setup to test but maybe it works

--- Models/Extensions.cs
+++ Models/Extensions.cs
@@ -1,4 +1,18 @@
-﻿namespace Datamatiker2SemExam.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Datamatiker2SemExam.Models
 {
-   
+    public partial class MassageDBContext : DbContext
+    {
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+            FileInfo file = new("ConnectionString");
+            string connect = file.OpenText().ReadToEnd();
+         optionsBuilder.UseSqlServer(connect);
+        }
+    }
 }
--- Models/MassageDBContext.cs
+++ Models/MassageDBContext.cs
@@ -33,9 +33,7 @@ public partial class MassageDBContext : DbContext
 
     public virtual DbSet<Worker> Workers { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB.;Initial Catalog=MassageDB;Integrated Security=True;Encrypt=True");
+    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
