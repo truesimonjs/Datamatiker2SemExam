5654b8c759baa275c9c7c511f980305eeb47275e
emrekaydk <emrekaydk@gmail.com>
2026-05-07 11:41:54 +0200

    s

--- Pages/ViewBooking.cshtml.cs
+++ Pages/ViewBooking.cshtml.cs
@@ -1,3 +1,4 @@
+using Datamatiker2SemExam.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,17 @@ namespace Datamatiker2SemExam.Pages
 {
     public class ViewBookingModel : PageModel
     {
+        private readonly MassageDBContext _context;
+
+        public ViewBookingModel(MassageDBContext context)
+        {
+            _context = context;
+        }
+
+        public List<Booking> Bookings { get; set; } = new List<Booking>();
         public void OnGet()
         {
+            Bookings = _context.Bookings.ToList();
         }
     }
 }
