cfff8e672066e7403c4d7759930a6cc9f77ea121
Burak0153 <but001@edu.zealand.dk>
2026-05-08 11:09:18 +0200

    slettede en kommentar

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -14,10 +14,7 @@ namespace Datamatiker2SemExam.Pages
         {
             using MassageDBContext context = new MassageDBContext();
             OpeningHours = context.OpeningHours.ToList();
-<<<<<<< HEAD
-=======
-            //Console.WriteLine(OpeningHours[0].Day); 
->>>>>>> ViewBooking
+
         }
     }
 }
