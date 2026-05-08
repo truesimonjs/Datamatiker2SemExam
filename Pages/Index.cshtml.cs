edbfdf0a65893819541d909529558aae6706609d
emrekaydk <emrekaydk@gmail.com>
2026-05-08 10:51:52 +0200

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -14,7 +14,7 @@ namespace Datamatiker2SemExam.Pages
         {
             using MassageDBContext context = new MassageDBContext();
             OpeningHours = context.OpeningHours.ToList();
-            Console.WriteLine(OpeningHours[0].Day); 
+            //Console.WriteLine(OpeningHours[0].Day); 
         }
     }
 }
