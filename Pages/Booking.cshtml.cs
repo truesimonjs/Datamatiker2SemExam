7ada84f3113dedd1b3a724ecb1556c90bd3e8a09
emrekaydk <emrekaydk@gmail.com>
2026-05-14 14:36:54 +0200

    ss
    SletBookingSide oprettet

--- Pages/Booking.cshtml.cs
+++ Pages/Booking.cshtml.cs
@@ -1,3 +1,5 @@
+using Datamatiker2SemExam.Interfaces;
+using Datamatiker2SemExam.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,11 +7,27 @@ namespace Datamatiker2SemExam.Pages
 {
     public class BookingModel : PageModel
     {
+        private readonly IBookingRepository _bookingRepository;
 
         public int Id { get; set; }
-        public void OnGet(int id)
+        public string Name { get; set; }
+        public Booking? Booking { get; set; }
+
+        public BookingModel(IBookingRepository bookingRepository)
+        {
+            _bookingRepository = bookingRepository;
+        }
+
+        public IActionResult OnGet(int id)
         {
             Id = id;
+            Booking = _bookingRepository.Read(id);
+            if (Booking == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
+   
     }
 }
